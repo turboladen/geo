ea9d8ed2310afe740d323c54f3addfba78d0e6d1
Steve Loveless <steve@agrian.com>
2018-07-30 12:26:00 -0700

    Line+Point intersection

--- geo/src/algorithm/intersection.rs
+++ geo/src/algorithm/intersection.rs
@@ -1,10 +1,18 @@
 use num_traits::Float;
-use geo_types::Point;
+use geo_types::{Line, Point};
 
 /// Describes the possible outcomes of intersecting a Point with another Geometry.
 ///
 #[derive(Debug, PartialEq)]
 pub enum PointIntersection<T: Float> {
+    Point(Point<T>),
+    None
+}
+
+/// Describes the possible outcomes of intersecting a Point with another Geometry.
+///
+#[derive(Debug, PartialEq)]
+pub enum LineIntersection<T: Float> {
     Point(Point<T>),
     None
 }
@@ -20,8 +28,8 @@ pub trait Intersection<Rhs = Self> {
 impl<T> Intersection for Point<T> where T: Float {
     type Output = PointIntersection<T>;
 
-    fn intersection(&self, other_point: &Point<T>) -> PointIntersection<T> {
-        if self.eq(other_point) {
+    fn intersection(&self, rhs: &Point<T>) -> Self::Output {
+        if self.eq(rhs) {
             PointIntersection::Point(self.clone())
         } else {
             PointIntersection::None
@@ -29,10 +37,22 @@ impl<T> Intersection for Point<T> where T: Float {
     }
 }
 
+impl<T> Intersection<Point<T>> for Line<T> where T: Float {
+    type Output = LineIntersection<T>;
+
+    fn intersection(&self, rhs: &Point<T>) -> Self::Output {
+        if [self.start, self.end].contains(&rhs.0) {
+            LineIntersection::Point(rhs.clone())
+        } else {
+            LineIntersection::None
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use geo_types::{Point};
-    use super::{Intersection, PointIntersection};
+    use geo_types::{Coordinate, Line, Point};
+    use super::{Intersection, LineIntersection, PointIntersection};
 
     #[test]
     fn point_with_intersecting_point() {
@@ -48,5 +68,42 @@ mod tests {
         let p2 = Point::new(2.0, 2.0);
 
         assert_eq!(p1.intersection(&p2), PointIntersection::None)
+    }
+
+    #[test]
+    fn line_with_intersecting_point() {
+        let line = Line::new(
+            Coordinate { x: 1.0, y: 1.0 },
+            Coordinate { x: 2.0, y: 2.0 },
+        );
+
+        let point = Point::new(1.0, 1.0);
+
+        assert_eq!(
+            line.intersection(&point),
+            LineIntersection::Point(point.clone())
+        );
+
+        let point = Point::new(2.0, 2.0);
+
+        assert_eq!(
+            line.intersection(&point),
+            LineIntersection::Point(point.clone())
+        );
+    }
+
+    #[test]
+    fn line_with_no_intersecting_point() {
+        let line = Line::new(
+            Coordinate { x: 10.0, y: 10.0 },
+            Coordinate { x: 20.0, y: 10.0 },
+        );
+
+        let point = Point::new(1.0, 1.0);
+
+        assert_eq!(
+            line.intersection(&point),
+            LineIntersection::None
+        );
     }
 }
