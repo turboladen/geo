8b898cfcd2130384c3a406a06a4172ac20130413
Steve Loveless <steve@agrian.com>
2018-07-30 17:02:01 -0700

    Implement Intersection for Point and MultiPolygon

--- geo/src/algorithm/intersection.rs
+++ geo/src/algorithm/intersection.rs
@@ -1,6 +1,6 @@
 use algorithm::intersects::Intersects;
 use num_traits::Float;
-use geo_types::{Line, LineString, MultiLineString, MultiPoint, Point, Polygon};
+use geo_types::{Line, LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon};
 
 /// Describes the possible outcomes of intersecting a Point with another Geometry.
 ///
@@ -46,6 +46,14 @@ pub enum MultiPointIntersection<T: Float> {
 ///
 #[derive(Debug, PartialEq)]
 pub enum MultiLineStringIntersection<T: Float> {
+    Point(Point<T>),
+    None
+}
+
+/// Describes the possible outcomes of intersecting a MultiPolygon with another Geometry.
+///
+#[derive(Debug, PartialEq)]
+pub enum MultiPolygonIntersection<T: Float> {
     Point(Point<T>),
     None
 }
@@ -181,6 +189,18 @@ impl<T> Intersection<Point<T>> for MultiLineString<T> where T: Float {
             MultiLineStringIntersection::Point(rhs.clone())
         } else {
             MultiLineStringIntersection::None
+        }
+    }
+}
+
+impl<T> Intersection<Point<T>> for MultiPolygon<T> where T: Float {
+    type Output = MultiPolygonIntersection<T>;
+
+    fn intersection(&self, rhs: &Point<T>) -> Self::Output {
+        if self.intersects(rhs) {
+            MultiPolygonIntersection::Point(rhs.clone())
+        } else {
+            MultiPolygonIntersection::None
         }
     }
 }
@@ -409,6 +429,139 @@ mod tests {
         assert_eq!(
             mls.intersection(&point),
             MultiLineStringIntersection::None,
+        );
+    }
+
+    #[test]
+    fn multipolygon_with_point() {
+        let polygon1 = {
+            let exterior = LineString(vec![
+                                      Coordinate { x: 0.0, y: 0.0 },
+                                      Coordinate { x: 1.0, y: 1.0 },
+                                      Coordinate { x: 1.0, y: 0.0 },
+                                      Coordinate { x: 0.0, y: 0.0 },
+            ]);
+            let interiors = vec![LineString(vec![
+                                            Coordinate { x: 0.1, y: 0.1 },
+                                            Coordinate { x: 0.9, y: 0.9 },
+                                            Coordinate { x: 0.9, y: 0.1 },
+                                            Coordinate { x: 0.1, y: 0.1 },
+            ])];
+            Polygon::new(exterior, interiors)
+        };
+
+        let polygon2 = {
+            let exterior = LineString(vec![
+                                      Coordinate { x: 100.0, y: 100.0 },
+                                      Coordinate { x: 101.0, y: 101.0 },
+                                      Coordinate { x: 101.0, y: 100.0 },
+                                      Coordinate { x: 100.0, y: 100.0 },
+            ]);
+            let interiors = vec![LineString(vec![
+                                            Coordinate { x: 100.1, y: 100.1 },
+                                            Coordinate { x: 100.9, y: 100.9 },
+                                            Coordinate { x: 100.9, y: 100.1 },
+                                            Coordinate { x: 100.1, y: 100.1 },
+            ])];
+            Polygon::new(exterior, interiors)
+        };
+
+        let multi_polygon = MultiPolygon(vec![polygon1, polygon2]);
+
+        //-----------------------------------------------------------------------------------------
+        // FIRST POLYGON
+        //-----------------------------------------------------------------------------------------
+        // Start of first Polygon exterior
+        let point = Point::new(0.0, 0.0);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        // On Polygon exterior
+        let point = Point::new(0.5, 0.0);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        // Start of first Polygon interior
+        let point = Point::new(0.1, 0.1);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        // On first Polygon interior
+        let point = Point::new(0.9, 0.4);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        // In first Polygon interior
+        let point = Point::new(0.05, 0.05);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        //-----------------------------------------------------------------------------------------
+        // SECOND POLYGON
+        //-----------------------------------------------------------------------------------------
+        // Start of second Polygon exterior
+        let point = Point::new(100.0, 100.0);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        // On Polygon exterior
+        let point = Point::new(100.5, 100.0);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        // Start of second Polygon interior
+        let point = Point::new(100.1, 100.1);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        // On second Polygon interior
+        let point = Point::new(100.9, 100.4);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        // In second Polygon interior
+        let point = Point::new(100.05, 100.05);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::Point(point.clone())
+        );
+
+        //-----------------------------------------------------------------------------------------
+        // Not in or on Polygon
+        //-----------------------------------------------------------------------------------------
+        let point = Point::new(10.0, 10.0);
+
+        assert_eq!(
+            multi_polygon.intersection(&point),
+            MultiPolygonIntersection::None,
         );
     }
 }
