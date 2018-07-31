014b8fcea14e9b738db3ef2263417782b9121505
Steve Loveless <steve@agrian.com>
2018-07-30 20:17:26 -0700

    Implement Intersection GeometryCollection and Point

--- geo/src/algorithm/intersection.rs
+++ geo/src/algorithm/intersection.rs
@@ -1,6 +1,6 @@
 use algorithm::intersects::Intersects;
 use num_traits::Float;
-use geo_types::{Line, LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon};
+use geo_types::{GeometryCollection, Line, LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon};
 
 /// Describes the possible outcomes of intersecting a Point with another Geometry.
 ///
@@ -54,6 +54,14 @@ pub enum MultiLineStringIntersection<T: Float> {
 ///
 #[derive(Debug, PartialEq)]
 pub enum MultiPolygonIntersection<T: Float> {
+    Point(Point<T>),
+    None
+}
+
+/// Describes the possible outcomes of intersecting a GeometryCollection with another Geometry.
+///
+#[derive(Debug, PartialEq)]
+pub enum GeometryCollectionIntersection<T: Float> {
     Point(Point<T>),
     None
 }
@@ -133,6 +141,28 @@ impl<T> Intersection<MultiLineString<T>> for Point<T> where T: Float {
     }
 }
 
+impl<T> Intersection<MultiPolygon<T>> for Point<T> where T: Float {
+    type Output = PointIntersection<T>;
+
+    fn intersection(&self, rhs: &MultiPolygon<T>) -> Self::Output {
+        match rhs.intersection(self) {
+            MultiPolygonIntersection::Point(p) => PointIntersection::Point(p.clone()),
+            _ => PointIntersection::None,
+        }
+    }
+}
+
+impl<T> Intersection<GeometryCollection<T>> for Point<T> where T: Float {
+    type Output = PointIntersection<T>;
+
+    fn intersection(&self, rhs: &GeometryCollection<T>) -> Self::Output {
+        match rhs.intersection(self) {
+            GeometryCollectionIntersection::Point(p) => PointIntersection::Point(p.clone()),
+            _ => PointIntersection::None,
+        }
+    }
+}
+
 impl<T> Intersection<Point<T>> for Line<T> where T: Float {
     type Output = LineIntersection<T>;
 
@@ -205,12 +235,24 @@ impl<T> Intersection<Point<T>> for MultiPolygon<T> where T: Float {
     }
 }
 
+impl<T> Intersection<Point<T>> for GeometryCollection<T> where T: Float {
+    type Output = GeometryCollectionIntersection<T>;
+
+    fn intersection(&self, rhs: &Point<T>) -> Self::Output {
+        if self.intersects(rhs) {
+            GeometryCollectionIntersection::Point(rhs.clone())
+        } else {
+            GeometryCollectionIntersection::None
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use geo_types::{Coordinate, Line, Point};
+    use geo_types::{Geometry, Coordinate, Line, Point};
     use super::*;
 
-    // Apparently line![] is already take?
+    // Apparently line![] is already taken?
     macro_rules! build_line {
         (
             ($x1:expr, $y1:expr), ($x2:expr, $y2:expr)
@@ -577,6 +619,132 @@ mod tests {
         assert_eq!(
             multi_polygon.intersection(&point),
             MultiPolygonIntersection::None,
+        );
+    }
+
+    #[test]
+    fn geometrycollection_with_point() {
+        let mut geometries: Vec<Geometry<f32>> = vec![];
+
+        geometries.push(Geometry::Point(Point::new(180.0, 180.0)));
+        geometries.push(Geometry::Line(build_line![(179.0, 179.0), (178.0, 179.0)]));
+        geometries.push(Geometry::LineString(line_string![(170.0, 170.0), (171.0, 170.0)]));
+
+        let polygon1 = {
+            let exterior = line_string![(0.0, 0.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)];
+            let interiors = vec![
+                line_string![(0.1, 0.1), (0.9, 0.9), (0.9, 0.1), (0.1, 0.1)],
+            ];
+            Polygon::new(exterior, interiors)
+        };
+
+        let polygon2 = {
+            let exterior = line_string![(100.0, 100.0), (101.0, 101.0), (101.0, 100.0), (100.0, 100.0)];
+
+            let interiors = vec![
+                line_string![(100.1, 100.1), (100.9, 100.9), (100.9, 100.1), (100.1, 100.1)],
+            ];
+            Polygon::new(exterior, interiors)
+        };
+
+        geometries.push(Geometry::MultiPolygon(MultiPolygon(vec![polygon1, polygon2])));
+
+        let geometry_collection = GeometryCollection(geometries);
+
+        //-----------------------------------------------------------------------------------------
+        // FIRST POLYGON
+        //-----------------------------------------------------------------------------------------
+        // Start of first Polygon exterior
+        let point = Point::new(0.0, 0.0);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        // On Polygon exterior
+        let point = Point::new(0.5, 0.0);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        // Start of first Polygon interior
+        let point = Point::new(0.1, 0.1);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        // On first Polygon interior
+        let point = Point::new(0.9, 0.4);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        // In first Polygon interior
+        let point = Point::new(0.05, 0.05);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        //-----------------------------------------------------------------------------------------
+        // SECOND POLYGON
+        //-----------------------------------------------------------------------------------------
+        // Start of second Polygon exterior
+        let point = Point::new(100.0, 100.0);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        // On Polygon exterior
+        let point = Point::new(100.5, 100.0);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        // Start of second Polygon interior
+        let point = Point::new(100.1, 100.1);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        // On second Polygon interior
+        let point = Point::new(100.9, 100.4);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        // In second Polygon interior
+        let point = Point::new(100.05, 100.05);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::Point(point.clone())
+        );
+
+        //-----------------------------------------------------------------------------------------
+        // Not in or on Polygon
+        //-----------------------------------------------------------------------------------------
+        let point = Point::new(10.0, 10.0);
+
+        assert_eq!(
+            geometry_collection.intersection(&point),
+            GeometryCollectionIntersection::None,
         );
     }
 }
